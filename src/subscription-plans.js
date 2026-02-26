94ba7e3be6b5147d36dd91a7bb5ff694f1709b2d
schanthawon-a11y <schanthawon@gmail.com>
2026-02-27 05:40:20 +0700

    Add subscription plans with free, monthly, and yearly options.

---
+++ src/subscription-plans.js
@@ -0,0 +1,29 @@
+const subscriptionPlans = {
+  free: {
+    price: 0,
+    features: [
+      'Access to basic features',
+      'Limited support',
+      'Community access',
+    ],
+  },
+  monthly: {
+    price: 9.99,
+    features: [
+      'Access to all features',
+      'Priority support',
+      'Monthly updates',
+    ],
+  },
+  yearly: {
+    price: 99.99,
+    features: [
+      'Access to all features',
+      'Priority support',
+      'Monthly updates',
+      '2 months free',
+    ],
+  },
+};
+
+export default subscriptionPlans;
